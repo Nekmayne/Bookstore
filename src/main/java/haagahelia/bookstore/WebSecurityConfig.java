a92a00499c1f09aa08629a9d8730f8c38008121d
Nekmayne <bgm035@myy.haaga-helia.fi>
2021-03-15 19:41:56 +0200

    heroku4

--- src/main/java/haagahelia/bookstore/WebSecurityConfig.java
+++ src/main/java/haagahelia/bookstore/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package haagahelia.bookstore;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,13 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.stereotype.Service;
-
-import haagahelia.bookstore.domain.UserRepository;
 
 @Configuration
 @EnableWebSecurity
